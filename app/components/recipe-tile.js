8a5d594322aefda540f8e8ba2bd8f5ab48556f76
kyle <altck8@gmail.com>
2016-03-07 13:55:01 -0800

    transition to new absolute api and added helper for capitializing first character of words

--- app/components/recipe-tile.js
+++ app/components/recipe-tile.js
@@ -2,108 +2,195 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   sampleResponse: {
-                    idDrink: "13060",
-                    strDrink: "Margarita",
-                    strCategory: "Ordinary Drink",
-                    strAlcoholic: "Alcoholic",
-                    strGlass: "Cocktail glass",
-                    strInstructions: "Rub rim of cocktail glass with lime juice, dip rim in salt. Shake all ingredients with ice, strain into the salt-rimmed glass, and serve.",
-                    strDrinkThumb: "http://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg",
-                    strIngredient1: "Tequila",
-                    strIngredient2: "Triple sec",
-                    strIngredient3: "Lime juice",
-                    strIngredient4: "Salt",
-                    strIngredient5: "",
-                    strIngredient6: "",
-                    strIngredient7: "",
-                    strIngredient8: "",
-                    strIngredient9: "",
-                    strIngredient10: "",
-                    strIngredient11: "",
-                    strIngredient12: "",
-                    strIngredient13: "",
-                    strIngredient14: "",
-                    strIngredient15: "",
-                    strMeasure1: "1 1/2 oz ",
-                    strMeasure2: "1/2 oz ",
-                    strMeasure3: "1 oz ",
-                    strMeasure4: " ",
-                    strMeasure5: " ",
-                    strMeasure6: " ",
-                    strMeasure7: " ",
-                    strMeasure8: " ",
-                    strMeasure9: " ",
-                    strMeasure10: "",
-                    strMeasure11: "",
-                    strMeasure12: "",
-                    strMeasure13: "",
-                    strMeasure14: "",
-                    strMeasure15: "",
-                    dateModified: "2015-08-18 14:42:59"
-                 },
+                     "description":"[Fill|Action|fill] a [shaker|Tool|boston-shaker] with [ice cubes|Ingredient|ice-cubes]. [Add|Action|add] all ingredients. [Shake|Action|shake] and [strain|Action|strain] into a chilled [cocktail glass|Glass|cocktail-glass]. [Garnish|Action|garnish] with orange.",
+                     "story": "A pink drink served in a Martini glass and called The Cosmopolitan is said to have originated in San Francisco in the 1980s, but it wasn't\r\nuntil 1988--the year Absolut Citron was launched--that the\r\nCosmopolitan as it is known today was created. Bartender Toby Cecchini\r\nadapted the original recipe, using fresh juice, orange liqueur, a\r\nsplash of cranberry, and the first citrus-flavored vodka. The rest is history!",
+                     "color": "Pink",
+                     "rating":87,
+                     "skill":{
+                        "id":"beginner",
+                        "name":"Beginner",
+                        "value":1
+                     },
+                     "videos":[
+                        {
+                           "video":"absolut-cosmopolitan.mp4",
+                           "type":"assets"
+                        },
+                        {
+                           "video":"pggWEFyT1Ds",
+                           "type":"youtube"
+                        }
+                     ],
+                     "isAlcoholic":true,
+                     "isCarbonated":false,
+                     "isHot": false,
+                     "tags":[
+                        {
+                           "Owner":"apps/225",
+                           "Name":"Coll_Citron"
+                        },
+                        {
+                           "Owner":"apps/225",
+                           "Name":"Coll_Classic1"
+                        },
+                        {
+                           "Owner":"apps/225",
+                           "Name":"Coll_Vodka"
+                        },
+                        {
+                           "Owner":"apps/225",
+                           "Name":"drinkspiration"
+                        }
+                     ],
+                     "servedIn":{
+                        "id":"cocktail-glass",
+                        "text":"Cocktail Glass"
+                     },
+                     "ingredients":[
+                        {
+                           "type":"ice",
+                           "id":"ice-cubes",
+                           "text":"[Ice Cubes]",
+                           "textPlain": "Ice Cubes"
+                        },
+                        {
+                           "type":"vodka",
+                           "id":"absolut-citron",
+                           "text":"2 Parts [ABSOLUT CITRON]",
+                           "textPlain": "2 Parts Absolut Citron"
+                        },
+                        {
+                           "type":"spirits-other",
+                           "id":"orange-liqueur",
+                           "text":"½ Part [Orange Liqueur]",
+                           "textPlain": "1 Part Orange Liqueur"
+
+                        },
+                        {
+                           "type":"mixers",
+                           "id":"cranberry-juice",
+                           "text":"½ Part [Cranberry Juice]",
+                           "textPlain": "½ Part Cranberry Juice"
+                        },
+                        {
+                           "type":"mixers",
+                           "id":"lime-juice",
+                           "text":"¼ Part [Lime Juice]",
+                           "textPlain": "1 Part Lime Juice"
+                        },
+                        {
+                           "type":"fruits",
+                           "id":"orange",
+                           "text":"1 Twist [Orange]",
+                           "textPlain": "1 Twist Orange"
+                        }
+                     ],
+                     "tastes":[
+                        {
+                           "id":"sweet",
+                           "text":"Sweet"
+                        },
+                        {
+                           "id":"fresh",
+                           "text":"Fresh"
+                        },
+                        {
+                           "id":"fruity",
+                           "text":"Fruity"
+                        }
+                     ],
+                     "occasions":[
+                        {
+                           "id":"afternoon",
+                           "text":"Afternoon Drinks"
+                        },
+                        {
+                           "id":"evening",
+                           "text":"Evening Drinks"
+                        },
+                        {
+                           "id":"pre-dinner",
+                           "text":"Pre-Dinner Drinks"
+                        }
+                     ],
+                     "tools":[
+                        {
+                           "id":"citrus-press",
+                           "text":"Citrus press"
+                        },
+                        {
+                           "id":"freezer",
+                           "text":"Freezer"
+                        },
+                        {
+                           "id":"jigger",
+                           "text":"Jigger"
+                        },
+                        {
+                           "id":"strainer",
+                           "text":"Strainer"
+                        },
+                        {
+                           "id":"twist-knife",
+                           "text":"Twist Knife"
+                        },
+                        {
+                           "id":"boston-shaker",
+                           "text":"Boston Shaker"
+                        }
+                     ],
+                     "drinkTypes":[],
+                     "actions":[
+                        {
+                           "id":"shake",
+                           "text":"Shake"
+                        },
+                        {
+                           "id":"strain",
+                           "text":"Strain"
+                        }
+                     ],
+                     "brands": [
+                           "absolut"
+                     ],
+                     "languageBranch":"en",
+                     "id":"absolut-cosmopolitan",
+                     "name":"ABSOLUT Cosmopolitan",
+                     "descriptionPlain": "Fill a shaker with ice cubes. Add all ingredients. Shake and strain into a chilled cocktail glass. Garnish with orange."
+                  },
 
   drinkName: Ember.computed( function( ) {
-    var response = this.get('sampleResponse');
-    return response.strDrink;
+    return this.get('sampleResponse').name;
   }),
   drinkCategory: Ember.computed( function( ) {
-    var response = this.get('sampleResponse');
-    return response.strCategory;
+    return this.get('sampleResponse').story;
   }),
   drinkAlcoholic: Ember.computed( function( ) {
-    var response = this.get('sampleResponse');
-    return response.strAlcoholic;
+    return( this.get('sampleResponse').isAlcoholic ) ? "Alcoholic" : "Non Alcoholic";
   }),
   drinkInstructions: Ember.computed( function ( ) {
-    var response = this.get('sampleResponse');
-    return response.strInstructions;
+    var r = this.get('sampleResponse').descriptionPlain.split( "." );
+    r.pop( );
+    return r;
   }),
-  drinkMeasures: Ember.computed( function( ) {
-    var response = this.get('sampleResponse');
-    var measures = [];
-    for( var m = 1; m <= 15; m++ ) {
-      var measureVarName = "strMeasure" + m.toString( );
-      var measureResponseValue = response[ measureVarName ];
-      if(measureResponseValue != " " && measureResponseValue != "") {
-        var splitMeasureResponse = measureResponseValue.split( " " );
-        var measurement = [];
-        for( var s = 0; s < splitMeasureResponse.length; s++ ) {
-          if(splitMeasureResponse[s] !== "") {
-            var measureAsNum = parseFloat(splitMeasureResponse[s]);
-            if(splitMeasureResponse[s] == "1/2") {
-              measurement.push("0.5");
-            }
-            else if(!isNaN(measureAsNum)) {
-              measurement.push(measureAsNum.toString( ));
-            }
-            else
-            {
-              measurement.push(splitMeasureResponse[s]);
-            }
-          }
-          else // Found end of this measure, calculate it as a whole.
-          {
-            var measureTotalAmount = 0;
-            var measureAs = "";
-            for( var g = 0; g < measurement.length; g++ ) {
-              measureAsNum = parseFloat(measurement[g]);
-              if( measurement[g] === "oz"){
-                measureAs = measurement[g];
-              }
-              else if(!isNaN(measureAsNum)) {
-                measureTotalAmount += measureAsNum;
-              }
-            }
-            measures.push( measureTotalAmount + " " + measureAs);
-            measureAs = "";
-            measureTotalAmount = "";
-          }
-        }
-      }
-    }
-    return measures;
+  drinkTastes: Ember.computed( function( ) {
+    return this.get('sampleResponse').tastes;
+  }),
+  drinkVideo: Ember.computed( function( ) {
+    return this.get('sampleResponse').videos[1].video;
+  }),
+  drinkTools: Ember.computed( function( ) {
+    return this.get('sampleResponse').tools;
+  }),
+  drinkServedIn: Ember.computed( function( ) {
+    return this.get('sampleResponse').servedIn.text;
+  }),
+  drinkOccasions: Ember.computed( function( ) {
+    return this.get('sampleResponse').occasions;
   }),
   drinkIngredients: Ember.computed( function( ) {
+    return this.get('sampleResponse').ingredients;
+    /*
     var response = this.get('sampleResponse');
     var ingredients = [];
     for( var i = 1; i <= 15; i++ ) {
@@ -114,5 +201,6 @@ export default Ember.Component.extend({
         }
     }
     return ingredients;
+    */
   }),
 });
---
+++ tests/unit/helpers/capitalize-helper-test.js
@@ -0,0 +1,10 @@
+import { capitalizeHelper } from 'cocktail/helpers/capitalize-helper';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | capitalize helper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = capitalizeHelper([42]);
+  assert.ok(result);
+});
