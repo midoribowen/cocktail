d131c2ae7cd211c5a6ddce72c599cce3541433fd
kyle <altck8@gmail.com>
2016-03-08 09:43:24 -0800

    start work on checkout-tile getting list from service

--- app/components/checkout-tile.js
+++ app/components/checkout-tile.js
@@ -2,16 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service('shopping-cart'),
-  actions: {
-    getTotalsFromList( ) {
-      var totals = {};
-      var checkOutList = this.get('shoppingCart').allIngredients;
-      for( var i = 0; i < checkOutList.length; i++ ) {
-        var parsedServing = parseInt( checkOutList[ i ][ 0 ] );
-        var id = checkOutList[ i ].id.toString( );
-        totals[ id ] = (totals[ id ] === undefined) ? parsedServing : totals[ id ] + parsedServing;
-      }
-      return totals;
-    }
+
+  inShoppingCart: Ember.computed('shoppingCart.items', function( ) {
+
+  });
   }
 });
+
+
+/**
+for( var i = 0; i < checkOutList.length; i++ ) {
+  var parsedServing = parseInt( checkOutList[ i ][ 0 ] );
+  var id = checkOutList[ i ].id.toString( );
+  totals[ id ] = (totals[ id ] === undefined) ? parsedServing : totals[ id ] + parsedServing;
+}
+return totals;
+*/
